e43410e7e48d4b79fe6f5e6e0c45393b75505ee4
Kelechi125 <kelechi125@yahoo.com>
2024-09-10 19:45:34 -0500

    Created a function that updates the count and price of selected tickets

--- script.js
+++ script.js
@@ -6,8 +6,20 @@ const movieSelect = document.getElementById("movie");
 
 const ticketPrice = +movieSelect.value;
 
+// Update total and count
+const updateSelectedCount = () => {
+    const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
 container.addEventListener("click", (event) => {
     if(event.target.classList.contains("seat") && !event.target.classList.contains("occupied")) {
         event.target.classList.toggle("selected");
+
+        updateSelectedCount();
     };
-})
+});
